03a29d22d222c78e2f904daa2b8833a9a832e883
Juan Pablo Kuganda <149925566+juanKuganda@users.noreply.github.com>
2025-05-22 10:42:47 +0800

    update vite path

--- resources/views/components/default-layout.blade.php
+++ resources/views/components/default-layout.blade.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @vite(['resources/css/app.css', 'resources/js/app.tsx'])
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
   <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css" />
